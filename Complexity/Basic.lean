4dc3a02b660291c7b2fa49ce515c16daef205408
crei <crei.github@gmail.com>
2025-10-13 14:40:18 +0000

    Cleanup

--- Complexity/Basic.lean
+++ Complexity/Basic.lean
@@ -1,6 +1,5 @@
 import Complexity.Dyadic
 import Complexity.Successor
 import Complexity.TuringMachine
-import Complexity.Normal
 import Complexity.OutputRedirect
-import Complexity.Composition
+import Complexity.TMComposition
