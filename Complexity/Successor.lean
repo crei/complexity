cf40da382403383561a5af084cf576db62ab1e5c
crei <crei.github@gmail.com>
2025-10-24 18:46:45 +0200

    Use Option for dya

--- Complexity/Successor.lean
+++ Complexity/Successor.lean
@@ -3,17 +3,20 @@ import Complexity.Dyadic
 
 import Mathlib
 
-def succ_transition : Transition 1 (Fin 2) BlankChar :=
+inductive OneTwo where
+  | one
+  | two
+
+def succ_transition : Transition 1 (Fin 2) (Option OneTwo) :=
   fun state symbols =>
     match state with
     | 0 => match symbols 0 with
-      | ' ' => (1, fun _ => ('1', none))
-      | '1' => (1, fun _ => ('2', none))
-      | '2' => (0, fun _ => ('1', some .right))
-      | c => (0, fun _ => (c, some .right)) -- should not happen
+      | none => (1, fun _ => (some .one, none))
+      | some .one => (1, fun _ => (some .two, none))
+      | some .two => (0, fun _ => (some .one, some .right))
     | 1 => (1, (symbols ·, none))
 
-theorem stop_state_inert (tapes : Fin 1 → Turing.Tape BlankChar) (n : ℕ) :
+theorem stop_state_inert (tapes : Fin 1 → Turing.Tape (Option OneTwo)) (n : ℕ) :
   succ_transition.n_steps ⟨1, tapes⟩ n = ⟨1, tapes⟩ := by
   induction n with
   | zero => rfl
@@ -23,25 +26,25 @@ theorem stop_state_inert (tapes : Fin 1 → Turing.Tape BlankChar) (n : ℕ) :
 
 -- A Turing machine that computes the successor of a
 -- reversely encoded dyadic number
-def succ_tm : TM 1 (Fin 2) BlankChar := {
+def succ_tm : TM 1 (Fin 2) (Option OneTwo) := {
   transition := succ_transition
   startState := 0
   stopState := 1
 }
 
-def rev_dya (n : ℕ) : List BlankChar :=
-  (dyadic_encoding_reverse n).map (fun x => if x then '2' else '1')
+def rev_dya (n : ℕ) : List OneTwo :=
+  (dyadic_encoding_reverse n).map (fun x => if x then .two else .one)
 
 @[simp]
 lemma rev_dya_zero : rev_dya 0 = [] := by
   simp [rev_dya, dyadic_encoding_reverse]
 
 @[simp]
-lemma rev_dya_odd (n : ℕ) : rev_dya (2 * n + 1) = '1' :: rev_dya (n) := by
+lemma rev_dya_odd (n : ℕ) : rev_dya (2 * n + 1) = .one :: rev_dya (n) := by
   simp [rev_dya, dyadic_encoding_reverse_prop_one]
 
 @[simp]
-lemma rev_dya_even (n : ℕ) : rev_dya (2 * n + 2) = '2' :: rev_dya (n) := by
+lemma rev_dya_even (n : ℕ) : rev_dya (2 * n + 2) = .two :: rev_dya (n) := by
   simp [rev_dya, dyadic_encoding_reverse_prop_two]
 
 
@@ -102,31 +105,4 @@ theorem succ_in_linear_time (n : ℕ) : succ_tm.runs_in_time
   use shift, .left;
   simp
 
-theorem listblank_cons_default_to_empty {Γ} [Inhabited Γ] :
-  (Turing.ListBlank.mk [default] : Turing.ListBlank Γ) = Turing.ListBlank.mk [] := by
-  have h2 : Turing.BlankRel ([default] : List Γ) [] := by
-    unfold Turing.BlankRel; right; use 1; simp
-  apply Quotient.sound
-  exact h2
-
-theorem listblank_all_blank_is_empty {Γ} [Inhabited Γ]
-  (list : List Γ)
-  (all_empty : ∀ i, list.getI i = default) :
-  Turing.ListBlank.mk list = Turing.ListBlank.mk [] := by
-  apply Turing.ListBlank.ext
-  simp [all_empty]
-
-theorem listblank_is_empty_all_blank {Γ} [Inhabited Γ]
-  (list : Turing.ListBlank Γ)
-  (is_empty : list = Turing.ListBlank.mk []) :
-  ∀ i, list.nth i = default := by
-  intro i
-  subst is_empty
-  simp_all only [List.getI_nil, Turing.ListBlank.nth_mk]
-
-lemma cons_is_empty {Γ} [Inhabited Γ]
-  (c : Γ)
-  (list : Turing.ListBlank Γ)
-  (is_empty : Turing.ListBlank.cons c list = Turing.ListBlank.mk []) :
-  c = default := by
-  simpa using listblank_is_empty_all_blank (Turing.ListBlank.cons c list ) is_empty 0
+-- theorem succ_in_dtime_id: dtime_nat id Nat.succ := by
