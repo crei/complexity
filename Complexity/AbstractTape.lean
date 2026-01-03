9ed7f7e14c8cb360f5bfc48118dd7a684288be93
crei <crei.github@gmail.com>
2026-01-03 17:09:15 +0100

    Sequential composition of Turing Machines.

--- Complexity/AbstractTape.lean
+++ Complexity/AbstractTape.lean
@@ -17,6 +17,11 @@ lemma move_int_nonneg {Γ} [Inhabited Γ]
   tape.move_int (Int.ofNat n) = (Turing.Tape.move .right)^[n] tape := by
   rfl
 
+lemma move_right_iter_eq_move_int {Γ} [Inhabited Γ]
+  (tape : Turing.Tape Γ) (n : ℕ) :
+  (Turing.Tape.move .right)^[n] tape = tape.move_int (Int.ofNat n) := by
+  rfl
+
 @[simp]
 lemma move_int_zero {Γ} [Inhabited Γ] (tape : Turing.Tape Γ) :
   tape.move_int 0 = tape := by
@@ -83,6 +88,11 @@ def Turing.Tape.write_at {Γ} [Inhabited Γ]
   (tape : Turing.Tape Γ) (pos : ℤ) (symbol : Γ) : Turing.Tape Γ :=
   ((tape.move_int pos).write symbol).move_int (-pos)
 
+lemma write_eq_write_at {Γ} [Inhabited Γ]
+  (tape : Turing.Tape Γ) (symbol : Γ) :
+  tape.write symbol = tape.write_at 0 symbol := by
+  rfl
+
 lemma write_at_move_int {Γ} [Inhabited Γ]
   (tape : Turing.Tape Γ) (pos shift : ℤ) (symbol : Γ) :
   (tape.write_at pos symbol).move_int shift =
@@ -90,7 +100,25 @@ lemma write_at_move_int {Γ} [Inhabited Γ]
   unfold Turing.Tape.write_at
   simp [Int.sub_eq_add_neg, Int.add_comm]
 
-lemma Turing.Tape.write_at_nth {Γ} [Inhabited Γ]
+lemma move_int_write_at {Γ} [Inhabited Γ]
+  (tape : Turing.Tape Γ) (pos shift : ℤ) (symbol : Γ) :
+  (tape.move_int shift).write_at pos symbol =
+    (tape.write_at (pos + shift) symbol).move_int shift := by
+  unfold Turing.Tape.write_at
+  simp [Int.add_comm]
+
+@[simp]
+lemma write_at_nth {Γ} [Inhabited Γ]
+  (tape : Turing.Tape Γ) (n : ℤ) :
+  (tape.write_at n (tape.nth n)) = tape := by
+  ext p
+  simp only [Turing.Tape.write_at, move_int_nth, Turing.Tape.write_nth,
+             neg_add_cancel_right, ite_eq_right_iff]
+  intro h_eq
+  have : p = n := by omega
+  rw [this]
+
+lemma Turing.Tape.write_at_tape_nth {Γ} [Inhabited Γ]
   (tape : Turing.Tape Γ) (p₁ p₂ : ℤ) (symbol : Γ) :
   (tape.write_at p₁ symbol).nth p₂ = if p₁ = p₂ then symbol else tape.nth p₂ := by
   unfold Turing.Tape.write_at
