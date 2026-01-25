66b2bb9a57f88827dbf1aac52fef7b106114cd85
crei <crei.github@gmail.com>
2026-01-25 23:15:55 +0100

    prog

--- Complexity/Pop.lean
+++ Complexity/Pop.lean
@@ -20,12 +20,13 @@ def pop := (Routines.while (· ≠ .sep) pop_char).seq pop_char
 theorem pop.eval {w : List Char} {ws₁ ws₂ : List (List Char)} :
   pop.eval (list_to_tape ∘ [w :: ws₁].get) =
     .some (list_to_tape ∘ [ws₁].get) := by
-  let semantics := fun tapes => (tapes 0).
   let tapes : ℕ → Fin 1 → Turing.Tape SChar :=
     (fun i j => Turing.Tape.mk₁ ((list_to_string (w :: ws₁)).drop i))
   have h_tapes₀ : tapes 0 = (list_to_tape ∘ [w :: ws₁].get) := by
-    sorry
+    funext i; simp [tapes, list_to_tape]
   have h_inner : ∀ i, pop_char.eval (tapes i) = tapes i.succ := by
+    induction with
+    | zero =>
     sorry
   let x := Routines.while.eval (condition := (· ≠ .sep)) (tm := pop_char) tapes h_inner
   simp at x
